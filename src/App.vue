7259aa7267c16b2dbc3d8cb8d2ec987ce904b01e
Sammy Relles <scr92@cornell.edu>
2025-01-30 18:31:08 +0000

    class work 1-30-25

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script setup>
 import IncreaseCount from './components/IncreaseCount.vue'
 import ShowCount from './components/ShowCount.vue'
+import { ref } from 'vue'
+
+const c = ref(0)
+
+function increaseCount(inc) {
+  c.value = c.value + Number(inc)
+}
 </script>
 
 <template>
   <div class="component">
-    <ShowCount />
-    <IncreaseCount />
+    <ShowCount v-bind:count="c" />
+    <IncreaseCount increment="1"
+    v-on:increase-by="increaseCount"/>
+    <IncreaseCount increment="10"
+    v-on:increase-by="increaseCount"/>
+    <IncreaseCount increment="100"
+    v-on:increase-by="increaseCount"/>
+    <!-- a reusable component to call -->
   </div>
 </template>
 
